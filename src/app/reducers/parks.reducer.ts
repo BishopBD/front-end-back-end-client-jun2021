0a9c48fbf967aec163368d108a8264dda57b2dfb
Jeffry Gonzalez <jeff@hypertheory.com>
2021-06-15 10:28:42 -0400

    beginning tuesday

--- src/app/reducers/parks.reducer.ts
+++ src/app/reducers/parks.reducer.ts
@@ -15,15 +15,15 @@ export interface ParksState extends EntityState<ParksModel> {
 export const adapter = createEntityAdapter<ParksModel>();
 
 /** this will give you an initial empty state */
-// const initialState = adapter.getInitialState();
+const initialState = adapter.getInitialState();
 /** this is for fake data */
-const initialState: ParksState = {
-  ids: ['1', '2'],
-  entities: {
-    '1': { id: '1', name: 'Guido\'s Park', acreage: '15 Acres', notes: 'Not a good place to be after dark.' },
-    '2': { id: '2', name: 'Bardo Creek', acreage: '132 Acres', notes: 'Bring your raft!' }
-  }
-}
+// const initialState: ParksState = {
+//   ids: ['1', '2'],
+//   entities: {
+//     '1': { id: '1', name: 'Guido\'s Park', acreage: '15 Acres', notes: 'Not a good place to be after dark.' },
+//     '2': { id: '2', name: 'Bardo Creek', acreage: '132 Acres', notes: 'Bring your raft!' }
+//   }
+// }
 
 const reducerFunction = createReducer(
   initialState
